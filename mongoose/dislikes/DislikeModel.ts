b16deb0f2b8b6920b2f92013d026cf8c6e0027c7
abdul <abdul.isegen@gmail.com>
2022-11-20 20:08:52 -0500

    added changes

--- mongoose/dislikes/DislikeModel.ts
+++ mongoose/dislikes/DislikeModel.ts
@@ -3,9 +3,9 @@
  * documents in the dislikes collection
  */
 
- import mongoose from "mongoose";
- import DislikeSchema from "./DislikeSchema";
- 
- const DislikeModel = mongoose.model('DislikeModel', DislikeSchema);
- 
- export default DislikeModel
+import mongoose from "mongoose";
+import DislikeSchema from "./DislikeSchema";
+
+const DislikeModel = mongoose.model('DislikeModel', DislikeSchema);
+
+export default DislikeModel
